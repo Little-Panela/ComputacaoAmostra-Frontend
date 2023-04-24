69de40f828e3610c3b4ea1f335609c86c45060f4
Ryan Oliveira <jryancoelho79@hotmail.com>
2023-04-24 13:12:00 -0300

    fix: section broken logos

--- src/components/sections/home/Logos.tsx
+++ src/components/sections/home/Logos.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function Logos() {
   return (
     <section className="bg-green-900 px-2 sm:px-6 lg:px-8">
-      <div className="mx-auto flex max-w-7xl justify-around py-10">
+      <div className="mx-auto flex flex-wrap max-w-7xl justify-around py-10 max-sm:flex-col max-sm:gap-5">
         <div className="flex flex-col items-center gap-2">
           <h2 className="text-3xl font-bold text-white">Realização</h2>
           {/* <span className="text-white">LOGO</span> */}
