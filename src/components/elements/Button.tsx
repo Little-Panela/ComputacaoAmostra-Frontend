9b99917f0a28603d63d1c79150d9d2ef5b409974
marco <marco19300008@aluno.cesupa.br>
2023-05-20 16:04:12 -0300

    :lipstick: refactor: button style

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -3,35 +3,35 @@ import { Slot } from "@radix-ui/react-slot";
 import { clsx } from "clsx";
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-	variant?: "primary" | "secondary";
-	children: ReactNode;
-	asChild?: boolean;
+  variant?: "primary" | "secondary";
+  children: ReactNode;
+  asChild?: boolean;
 }
 
 export function Button({
-	variant = "primary",
-	children,
-	asChild,
-	className,
-	...props
+  variant = "primary",
+  children,
+  asChild,
+  className,
+  ...props
 }: ButtonProps) {
-	const Comp = asChild ? Slot : "button";
+  const Comp = asChild ? Slot : "button";
 
-	return (
-		<Comp
-			className={clsx(
-				{"box-border w-32 flex justify-center items-center transition-colors outline-none ring-pallete-primary focus:ring-2": variant === "primary"})}
-			{...props}
-		>
-			<div className={clsx(
-				{"flex justify-center items-center w-full h-full px-7 py-3 gap-2 font-montserrat text-sm text-white font-bold border-2 border-pallete-primary rounded-full \
-					hover:bg-pallete-primary-dark hover:border-pallete-primary-dark \
-					active:bg-pallete-primary-xdark active:border-pallete-primary-xdark \
-					transition-colors": variant === "primary"},
-				className
-			)}>
-				{children}
-			</div>
-		</Comp>
-	);
+  return (
+    <Comp
+      className={clsx(
+        {
+          "box-border w-fit flex gap-2 rounded-full border-2 border-pallete-primary px-7 py-3 font-montserrat text-sm font-bold text-white \
+					transition-colors hover:border-pallete-primary-dark \
+					hover:bg-pallete-primary-dark active:border-pallete-primary-xdark \
+					active:bg-pallete-primary-xdark \
+					focus:outline-3 outline-white ring-transparent": variant === "primary",
+        },
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </Comp>
+  );
 }
