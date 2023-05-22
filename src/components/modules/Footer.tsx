b10357e354e6917a29eda1e385c572c519a1c493
DanielMarcelino65 <danmarcelino6573@gmail.com>
2023-05-22 02:48:57 -0300

    :lipstick: feat Footer adaptation to Zenith Style

--- src/components/modules/Footer.tsx
+++ src/components/modules/Footer.tsx
@@ -1,68 +1,107 @@
 import { useTranslation } from "next-i18next";
 import Link from "next/link";
 import { LanguageSelector } from "./LanguageSelector";
+import { Text } from "../elements/Text";
 
 /* eslint-disable @next/next/no-img-element */
 export function Footer() {
   const { t } = useTranslation("common");
 
   return (
-    <footer className="bg-white">
-      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
-        <div
-          className="-mb-6 flex justify-center space-x-12"
-          aria-label="Footer"
-        >
-          <div className="flex items-center gap-2">
+    <footer className="bg-pallete-background- relative overflow-hidden">
+      <div className="pt-10 lg:pt-0 flex h-auto w-full flex-col items-center">
+        <div className="flex flex-col w-full lg:flex-row lg:justify-around">
+          <div className="flex justify-center items-center flex-col">
             <img
               src="/static/img/logo-comp-amostra.png"
               alt="logo computação amostra"
-              className="w-44"
+              className="h-auto w-[319px]"
             />
+            <div className="mb-10 mt-10 lg:mb-0 flex items-center justify-center space-x-10">
+              <a
+                href="https://www.facebook.com/CesupaOnline"
+                target="_blank"
+                rel="noreferrer"
+                className="z-[2]"
+              >
+                <img src="/static/img/facebook-white.svg" alt="facebook logo" />
+              </a>
+              <a
+                href="https://www.instagram.com/cesupaonline/"
+                target="_blank"
+                rel="noreferrer"
+                className="z-[2]"
+              >
+                <img
+                  src="/static/img/instagram-white.svg"
+                  alt="instagram logo"
+                />
+              </a>
+              <a
+                href="https://www.youtube.com/c/CesupaOnline"
+                target="_blank"
+                rel="noreferrer"
+                className="z-[2]"
+              >
+                <img src="/static/img/youtube-white.svg" alt="youtube logo" />
+              </a>
+            </div>
+          </div>
+          <div className="flex items-center w-auto box-border lg:pt-[110px] justify-between flex-col">
+            <LanguageSelector />
+            <div className="hidden mt-3 lg:flex items-center justify-center lg:w-[320px]">
+              <Link className="z-[2] pr-[5px]" href="/legal/privacy-policy">
+                <p className="text-xs font-bold leading-5 text-white transition-colors hover:text-gray-400">
+                  {t("footer.privacyPolicy")}
+                </p>
+              </Link>
+              <div className="divide-white font-bold">|</div>
+              <Link className="z-[2] pl-[5px]" href="/legal/terms">
+                <p className="text-xs font-bold leading-5 text-white transition-colors hover:text-gray-400">
+                  {t("footer.terms")}
+                </p>
+              </Link>
+            </div>
+          </div>
+
+          <div className="flex lg:w-[320px] flex-col items-center justify-center">
+            <img
+              src="/static/img/zenith.svg"
+              alt="logo zenith"
+              className="mt-10 h-[172px] w-[202px]"
+            />
+            <Text className="mb-[60px] font-bold">
+              EM PARCERIA COM PANIC <a className="text-red-600">♥</a>
+            </Text>
           </div>
         </div>
-        <div className="mt-10 flex items-center justify-center space-x-10">
-          <a
-            href="https://www.facebook.com/CesupaOnline"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src="/static/icons/facebook.svg" alt="facebook logo" />
-          </a>
-          <a
-            href="https://www.instagram.com/cesupaonline/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src="/static/icons/instagram.svg" alt="instagram logo" />
-          </a>
-          <a
-            href="https://www.youtube.com/c/CesupaOnline"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src="/static/icons/youtube.svg" alt="youtube logo" />
+        <div className="mt-3 lg:hidden flex flex-wrap items-center justify-center space-x-7">
+              <Link className="z-[2]" href="/legal/privacy-policy">
+                <p className="text-xs font-bold leading-5 text-white transition-colors hover:text-gray-400">
+                  {t("footer.privacyPolicy")}
+                </p>
+              </Link>
+              <div className="divide-white font-bold">|</div>
+              <Link className="z-[2]" href="/legal/terms">
+                <p className="text-xs font-bold leading-5 text-white transition-colors hover:text-gray-400">
+                  {t("footer.terms")}
+                </p>
+              </Link>
+        </div>
+        <div className="z-[2] mt-10 flex h-[60px] w-full items-center justify-center border-t-2 border-t-pallete-primary bg-black">
+          <a className="text-center text-xs leading-5 text-gray-500">
+            {t("footer.copyrightBefore")}
+            <a className="text-center text-xs leading-5 text-pallete-primary">
+              {t("footer.copyrightCesupa")}
+            </a>
+            {t("footer.copyrightAfter")}
           </a>
         </div>
-        <p className="mt-10 text-center text-xs leading-5 text-gray-500">
-          {t("footer.copyright")}
-        </p>
-        <div className="mt-3 flex flex-wrap items-center justify-center space-x-7">
-          <Link href="/legal/privacy-policy">
-            <p className="text-xs leading-5 text-gray-500 transition-colors hover:text-gray-400">
-              {t("footer.privacyPolicy")}
-            </p>
-          </Link>
-          <Link href="/legal/terms">
-            <p className="text-xs leading-5 text-gray-500 transition-colors hover:text-gray-400">
-              {t("footer.terms")}
-            </p>
-          </Link>
-        </div>
-        <p className="mt-10 text-center text-xs leading-5 text-gray-500">
-          {t("footer.developed")}
-        </p>
-        <LanguageSelector />
+        <img
+          src="/static/img/zenith-light.png"
+          alt="zenith-light"
+          className="absolute lg:w-[10000px] lg:top-[-300px] lg:h-[500px] bottom-[-200px] z-[0] h-auto w-auto scale-150 transform"
+        />
       </div>
     </footer>
   );
