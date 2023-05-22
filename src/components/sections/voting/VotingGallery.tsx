f8e095c5a444302d60a384cdefbc1ac7813789f7
marco <marco19300008@aluno.cesupa.br>
2023-05-22 11:46:31 -0300

    :recycle: style: projects page

--- src/components/sections/voting/VotingGallery.tsx
+++ src/components/sections/voting/VotingGallery.tsx
@@ -9,6 +9,7 @@ import { Heading } from "../../elements/Heading";
 import { Text } from "../../elements/Text";
 
 import type { TProject } from "../../../@types/TProject";
+import { useRouter } from "next/router";
 
 type VotingGalleryProps = {
   projects?: TProject[];
@@ -22,7 +23,8 @@ export function VotingGallery({
   isLoading,
 }: VotingGalleryProps) {
   const { t } = useTranslation("common");
-
+  const router = useRouter();
+  const locale = router.locale ?? "pt";
   return (
     <section className="relative mx-5 py-20 pb-40 mt-12 flex flex-col items-center gap-6 md:mt-8 md:gap-16">
       <img src="/static/img/video-wave.png" alt="Onda da Direita" className="absolute h-[600px] xl:h-[800px] top-40 -right-10 opacity-20 rotate-[-135deg] z-[2]" />
