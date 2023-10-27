acfa42f9b03b94afd624ef666369bd455f8e584a
Lucas Carvalho <112731358+Lucas-CarvalhoK@users.noreply.github.com>
2023-10-27 19:49:31 -0300

    :recycle: implementação do modal e tradução dos textos

--- src/components/sections/home/PremioMMIB.tsx
+++ src/components/sections/home/PremioMMIB.tsx
@@ -2,11 +2,14 @@
 import { useTranslation } from "react-i18next";
 import { Button } from "../../elements/Button";
 import { Heading } from "../../elements/Heading";
+import { ModalProject } from "../../modules/ModalProject";
 
 
 // eslint-disable-next-line @next/next/no-img-element
-export function PremioMMIB () {
+export function PremioMMIB () { 
     const { t } = useTranslation("common");
+    const AranduTeam = ["Bruna Melido", "Isadora Lacerda", "Ícaro Macedo"]
+      
 
     return (
         <section className="w-full flex flex-col justify-center items-center h-fit box-border pt-7 pb-7 bg-cover no-repeat bg-center lg:pb-56 lg:pt-56 lg:bg-mmib-desktop">
@@ -21,16 +24,31 @@ export function PremioMMIB () {
                 {/* div texto */}
                 <div className="flex flex-col gap-4 justify-center items-center">
                     <div className="flex border-b-2  border-pallete-primary">
-                        <Heading size="xl">Prêmio MMIB</Heading>
+                        <Heading size="xl">{t("awardMMIB.award")}</Heading>
                     </div>
-                    <p className="flex flex-wrap max-w-[60%] text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <p className="flex flex-wrap max-w-[60%] text-center">{t("awardMMIB.mainText")}</p>
                 </div>
                 <img
                 src="/static/img/Arandu.svg"
                 alt="Logo Arandu"
                 className="h-auto flex w-auto"
                 />
-                <Button>Ver Detalhes</Button>
+                <ModalProject
+                    name={t("winners-att.2023.bcc.thirdPlace.title")}
+                    nameForSlug={t("winners-att.2023.bcc.thirdPlace.title")}
+                    description={t("winners-att.2023.bcc.thirdPlace.modalDescription")}
+                    teamMembers={AranduTeam}
+                    isWinner={true}
+                    isZenith={false}
+                    isPublicVote={true}
+                    videoId={t("winners-att.2023.bcc.thirdPlace.youtubeId")}
+                    course={"ecomp"}
+                     trigger={
+                        <Button className="block w-fit rounded-2xl border-[1px] border-pallete-primary bg-transparent py-3 text-sm font-bold text-white">
+                            {t("home.winners.details")}
+                        </Button>
+                     }
+                    />
                 </div>
             </div>
 
@@ -38,10 +56,25 @@ export function PremioMMIB () {
             <div className="hidden lg:flex lg:flex-row lg:justify-between lg:pl-24 lg:pr-24 lg:w-full lg:max-w-screen-2xl">
                 <div className="flex flex-col gap-12 justify-center items-start z-10">
                     <div className="flex border-b-2  border-pallete-primary">
-                        <Heading size="2xl">Prêmio MMIB</Heading>
+                        <Heading size="2xl">{t("awardMMIB.award")}</Heading>
                     </div>
-                    <p className="flex max-w-[80%] flex-wrap text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-                    <Button>Ver Detalhes</Button>
+                    <p className="flex max-w-[80%] flex-wrap text-left">{t("awardMMIB.mainText")}</p>
+                    <ModalProject
+                    name={t("winners-att.2023.bcc.thirdPlace.title")}
+                    nameForSlug={t("winners-att.2023.bcc.thirdPlace.title")}
+                    description={t("winners-att.2023.bcc.thirdPlace.modalDescription")}
+                    teamMembers={AranduTeam}
+                    isWinner={true}
+                    isZenith={false}
+                    isPublicVote={true}
+                    videoId={t("winners-att.2023.bcc.thirdPlace.youtubeId")}
+                    course={"ecomp"}
+                     trigger={
+                        <Button className="block w-fit rounded-2xl border-[1px] border-pallete-primary bg-transparent py-3 text-sm font-bold text-white">
+                            {t("home.winners.details")}
+                        </Button>
+                     }
+                    />
                 </div>
                 <img
                 src="/static/img/ARANDU.svg"
